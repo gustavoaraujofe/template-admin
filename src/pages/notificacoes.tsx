3c9a59afd80c330bbe25c6b6f56c246391c8b2de
Gustavo Araújo <gustavoaraujofe@gmail.com>
2021-12-29 21:59:28 -0300

    Context API modo dark

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/template/Layout";
 export default function Notificacoes() {
   return (
     <Layout titulo="Notificações" subtitulo="Gerenciar notificacoes">
-      Conteudo
+      <h3>Conteudo</h3>
     </Layout>
   );
 }
